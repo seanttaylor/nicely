79facc2b8e529a3a12903f13aeb5f61d54e4bced
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2020-11-25 19:08:11 -0500

    ADDED post_likes AND comment_likes SQL TABLES Scaffolded project README

---
+++ README.md
@@ -0,0 +1,23 @@
+## Nicely
+
+#### Local Development
+
+
+#### Deployment
+
+
+#### Authentication
+
+
+#### Authorization
+
+
+#### Technology Credits
+Key software modules and services used regularly for designing, building and maintaining the Nicely platform are listed below. While they have been used specifically for addressing business use cases unique to Nicely, these are excellent tools in their own right and well worth the time to check out.
+
+* [SqlDbm](sqldbm.com) ~ a visual editor for creating relational database schemas, generates SQL schemas from the browser
+* [DB Fiddle](https://www.db-fiddle.com) ~ a browser-based playground for debugging SQL in a number of common dialects
+* [Stoplight](https://stoplight.io) ~ a visual editor for creating OpenAPI schema documents
+* [accesscontrol](https://www.npmjs.com/package/accesscontrol) ~ an NPM module offering basic role-based access control
+
+#### References
--- sqls/init.sql
+++ sqls/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS asiago;
 USE asiago;
 
 
+
 CREATE TABLE IF NOT EXISTS `users`
 (
  `id`             varchar(64) NOT NULL ,
@@ -82,12 +83,39 @@ KEY `fkIdx_197` (`user_id`),
 CONSTRAINT `FK_197` FOREIGN KEY `fkIdx_197` (`user_id`) REFERENCES `users` (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `comment_likes`
+(
+ `comment_id` varchar(64) NOT NULL ,
+ `user_id`    varchar(64) NOT NULL ,
+
+KEY `fkIdx_222` (`comment_id`),
+CONSTRAINT `FK_222` FOREIGN KEY `fkIdx_222` (`comment_id`) REFERENCES `comments` (`id`),
+KEY `fkIdx_225` (`user_id`),
+CONSTRAINT `FK_225` FOREIGN KEY `fkIdx_225` (`user_id`) REFERENCES `users` (`id`),
+UNIQUE INDEX(`comment_id`, `user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `post_likes`
+(
+ `post_id` varchar(64) NOT NULL ,
+ `user_id` varchar(64) NOT NULL ,
+
+KEY `fkIdx_215` (`post_id`),
+CONSTRAINT `FK_215` FOREIGN KEY `fkIdx_215` (`post_id`) REFERENCES `posts` (`id`),
+KEY `fkIdx_218` (`user_id`),
+CONSTRAINT `FK_218` FOREIGN KEY `fkIdx_218` (`user_id`) REFERENCES `users` (`id`),
+  UNIQUE INDEX(`post_id`, `user_id`)
+);
+
+
 
 INSERT INTO users (id, handle, email_address, phone_number, first_name, last_name, created_date) VALUES("e98417a8-d912-44e0-8d37-abe712ca840f", "@tstark", "tstark@avengers.io", "12125552424", "Tony", "Stark", "2020-09-26T23:08:27.645Z");
 
 INSERT INTO users (id, handle, email_address, phone_number, first_name, last_name, created_date) VALUES("b0a2ca71-475d-4a4e-8f5b-5a4ed9496a09", "@thor", "thor@avengers.io", "12125552020", "Thor", "Odinson", "2020-09-26T23:08:27.645Z");
 
 INSERT INTO posts (id, user_id, body, created_date) VALUES("d343fd4b-654a-40ae-bb54-05dd1f5b4791", "e98417a8-d912-44e0-8d37-abe712ca840f", "Playboy. Billionaire. Genius.", "2020-09-26T23:08:27.645Z");
+
+INSERT INTO posts (id, user_id, body, created_date, is_published) VALUES("f4c5c046-5cbb-4619-8358-639d2c0e898b", "e98417a8-d912-44e0-8d37-abe712ca840f", "Is it better to be feared or respected? I say: it is too much to ask for both?", "2020-09-26T23:08:27.645Z", 1);
 
 INSERT INTO comments (id, post_id, user_id, body, created_date) VALUES("a3a8b0dc-abb8-47d9-9545-00e6c24cc12d", "d343fd4b-654a-40ae-bb54-05dd1f5b4791", "@thor", "Yawn.", "2020-09-26T23:08:27.645Z");
 
@@ -100,4 +128,6 @@ INSERT INTO user_roles (user_id, role) VALUES("e98417a8-d912-44e0-8d37-abe712ca8
 INSERT INTO user_credentials (user_email_address, user_password) VALUES("tstark@avengers.io", "$2y$10$Zgd2BY7KhfZ0p2fpzB/fGeJIPRIOd4UKmZuQC.0f4kjIXjFi1VsSu");
 
 INSERT INTO user_credentials (user_email_address, user_password) VALUES("thor@avengers.io", "$2y$10$JGt.9x9ZOOZ7//S5fSRo7uKOck96G3Nz18aZ7oZ9pGqD8z7pPbR7O");
+
+INSERT INTO post_likes (post_id, user_id) VALUES("f4c5c046-5cbb-4619-8358-639d2c0e898b", "b0a2ca71-475d-4a4e-8f5b-5a4ed9496a09");
 
